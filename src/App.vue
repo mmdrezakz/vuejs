0786ef1c4182d51aed7e0156d410dba1b92903b7
Mohammadreza <mellboybeatprod@gmail.com>
2025-12-24 03:02:30 +0330

    Add Form Login

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,42 @@
 <script setup>
 import { ref } from 'vue';
 
-  const num = ref(0)
 
-  function Increasement(){
-    return num.value ++
-  }
-  function Decreasement(){
-    if(num.value !== 0 ){
-
-      return num.value --
-    }
-  }
-  const data =ref({id:1,name:"ahmad",age:34})
   const username = ref("")
+  const email = ref("")
+  const Users = ref([])
+
+  function Submithandler(){
+
+    Users.value.push({name:username.value,emailn:email.value})
+    email.value = ""
+    username.value = ""
+
+  }
 </script>
 
-<template>
-  <div>
-    <input class="rounded-2xl ring-4 ring-blue-600" type="text" v-model="username"/>
-    <p>{{username}}</p>
-    <div>
-      <ul>
-        <li  v-for="(value,key) in data" v-bind:key="key">{{key}} - {{value}}</li>
-      </ul>
-    </div>
-    <form  class="flex flex-col justify-center items-center w-full h-screen">
-      <button @click.prevent="Increasement" class="bg-blue-600 p-2 rounded-2xl">Increase</button>
-      <p  class="my-5 px-2 rounded-2xl ring-2 ring-blue-500 text-center">{{num}}</p>
-      <button @click.prevent="Decreasement" class="bg-blue-600 p-2 rounded-2xl">Decrease</button>
+<template class="bg-amber-100 w-full h-screen">
+  <div class="flex justify-center items-center m-auto w-full h-[80vh]" >
+    <form @submit.prevent="Submithandler" class="flex flex-col justify-center items-center bg-purple-200 p-10 rounded-2xl w-fit" >
+      <div class="flex justify-center gap-3 my-2">
+        <label class="w-24">UserName</label>
+        <input class="bg-white mx-4 px-2 rounded-2xl" type="text" placeholder="UserName" v-model="username"/>
+      </div>
+        <div class="flex justify-center gap-3 my-2">
+        <label class="w-24">Email</label>
+        <input class="bg-white mx-4 px-2 rounded-2xl" type="text" placeholder="Email" v-model="email"/>
+      </div>
+      <div class="w-full">
 
+        <button  v-if="username && email ? true : false" class="bg-blue-400 my-2 px-3 py-1 rounded-2xl">Send</button>
+        <p class="bg-white my-2 px-2 py-1 rounded-2xl" v-else>Please Enter username and email .</p>
+      </div>
+      <div>
+        <ul v-show="Users.length > 0">
+          <h2>Users :</h2>
+          <li  v-for="user in Users" v-bind:key="user">{{user.name}} --- {{user.emailn}}</li>
+        </ul>
+      </div>
     </form>
   </div>
 </template>
